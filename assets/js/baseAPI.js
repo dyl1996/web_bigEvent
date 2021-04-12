e6f2c8e173974e2ecf0724d30bc75991882223e6
dyl <duyanleil@yeah.net>
2021-04-12 13:15:10 +0800

    首页功能完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,5 +1,18 @@
 $.ajaxPrefilter(function (options) {
     // console.log(options.url); 
-    options.url = "http://www.liulongbin.top:3007" + options.url;
-    console.log(options.url);
+    options.url = "http://api-breakingnews-web.itheima.net" + options.url;
+    // token验证
+    if (options.url.indexOf("/my/") !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem("token") || ""
+        }
+        // 后台验证
+        options.complete = function (res) {
+            // console.log(res);
+            if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+                localStorage.removeItem("token");
+                location.href = 'login.html';
+            }
+        }
+    }
 });
---
+++ assets/js/index.js
@@ -0,0 +1,42 @@
+$(function () {
+    var layer = layui.layer;
+    // 获取用户信息
+    initUserInfo();
+    function initUserInfo() {
+        // $.ajax({
+        //     method: "GET",
+        //     url: "/my/userinfo",
+        //     success: function (res) {
+        //         console.log(res);
+        //     }
+        // });
+        $.get("/my/userinfo", function (res) {
+            // console.log(res);
+            if (res.status !== 0) return layer.msg(res.message);
+            // 渲染头像和昵称
+            renderAvatar(res.data);
+        });
+    }
+    // 渲染函数
+    function renderAvatar(user) {
+        var uname = user.nickname || user.username;
+        $(".welcome").html("欢迎 " + uname);
+        if (user.user_pic !== null) {
+            // 渲染图片头像
+            $(".pic_avatar").prop("src", user.user_pic).show();
+            $(".user_avatar").hide();
+        } else {
+            // 渲染文本呢头像
+            $(".pic_avatar").hide();
+            $(".user_avatar").html(uname[0].toUpperCase()).show();
+        }
+    }
+    // 退出功能
+    $(".btnSignout").on("click", () => {
+        layer.confirm("确定退出？", { icon: 3, title: '提示' }, index => {
+            localStorage.removeItem("token");
+            location.href = 'login.html';
+            layer.close(index);
+        });
+    });
+})
